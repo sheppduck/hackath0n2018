654ad5ec21f1da6bf0841976e8e5bf198b699b7e
sheppduck <shepp67@gmail.com>
2018-05-04 17:19:10 -0400

    WIP: rearranged the file structure, moved shit around

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,20 +8,6 @@ OC_PW="devops123!"
 OC_PROJECT="myhub"
 OC_CP_TMP="/tmp"
 OC_TAR_DIR="/temp/hackathon2018"
-# switch to the myhub project run oc to get PODS from myhub ns grep for solr
-# and save output as a variable
-oc login $OC_SERVER:8443 --username=$OC_UN --password=$OC_PW --insecure-skip-tls-verify
-oc project $OC_PROJECT
-## Grep for and add some add'l HUB PODs - how to use $1, $2, etc. for POD name vars?
-
-# Get all Namespaces and stuff into an array, trim the first line off the oc get
-ARRAY_OF_NS=(`oc get ns | cut -d ' ' -f1 | awk '{if(NR>1)print}'`)
-# Loop through the array and spit out all the projects
-for i in "${ARRAY_OF_NS[@]}"
-do
-  echo "$i is an oc project"
-done
-
 NS=$1
 PODS=$2
 OC_OUT_DIR=$3
@@ -29,7 +15,18 @@ WEBAPP="$(oc get pods | grep webapp | cut -d ' ' -f1)"
 echo "$WEBAPP"
 SOLR="$(oc get pods | grep solr | cut -d ' ' -f1)"
 echo "$SOLR"
-
+# Get all Namespaces and stuff then into an array, trim the first line off the oc get
+ARRAY_OF_NS=(`oc get ns | cut -d ' ' -f1 | awk '{if(NR>1)print}'`)
+# switch to the myhub project run oc to get PODS from myhub ns grep for solr
+# and save output as a variable
+oc login $OC_SERVER:8443 --username=$OC_UN --password=$OC_PW --insecure-skip-tls-verify
+oc project $OC_PROJECT
+## Grep for and add some add'l HUB PODs - how to use $1, $2, etc. for POD name vars?
+# Loop through the array and spit out all the projects
+for i in "${ARRAY_OF_NS[@]}"
+do
+  echo "$i is an oc project"
+done
 # now let's copy some files out of the hub-solr container
 # oc cp usage is 'oc cp <namespace/project>/POD:files_to_copy_from_pod /save/files/to/local/dir'
 # ... flesh this part out ... for other containers, get /bin of any test container...
@@ -39,7 +36,6 @@ oc cp $OC_PROJECT/$SOLR:/opt/solr/ $OC_CP_TMP
 oc cp $OC_PROJECT/$SOLR:/bin/ $OC_CP_TMP
 # Example?
 # oc cp $1/$2:/bin
-
 
 cd /tmp && ls -l
 mkdir -p $OC_TAR_DIR
